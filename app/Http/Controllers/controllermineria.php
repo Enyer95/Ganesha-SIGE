058e162e26e168454a6b029558b8ca88d4b61828
enyer95 <enyerfreitez@gmail.com>
2018-05-07 07:36:33 -0400

    s

--- app/Http/Controllers/controllermineria.php
+++ app/Http/Controllers/controllermineria.php
@@ -26,7 +26,7 @@ class controllermineria extends Controller
 		$results = DB::select('SELECT '.$request->newQuery.' OFFSET '.$request->minimo.' ROWS FETCH NEXT '.$cantidad.' ROWS ONLY;');
 		$discretizacion = ModelMineria::discretizacion($cabeceras,$cantidad,DB::select('SELECT '.$request->newQuery.' OFFSET '.$request->minimo.' ROWS FETCH NEXT '.$cantidad.' ROWS ONLY;'));
 		#dd($discretizacion);
-		#$nosmalizacion = ModelMineria::normalizacion($cantidad,$discretizacion,$cabeceras);
+		$nosmalizacion = ModelMineria::normalizacion($cantidad,$discretizacion,$cabeceras);
 		#dd($discretizacion[0]->cod_seccion);
     return view('Mineria/Mostrar_mineria')->with(['discretizacion' => $discretizacion[0],'normal'=>$results,'celdas'=>$cabeceras]);
 
